ee81ef6919572d436032fed3031b9b030369bd0c
Jonathan Martel-Raiche <martel-raiche.jonathan@courrier.uqam.ca>
2021-11-26 16:55:15 -0500

    Modification nom

--- server.js
+++ server.js
@@ -13,3 +13,11 @@ app.get('/*', (req, res) =>
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+/* const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/myapp'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname + '/dist/myapp/index.html'));});
+app.listen(process.env.PORT || 8080); */
