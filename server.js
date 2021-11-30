11ab808444b5d818b0e26f136ddeca4ec4bd67b9
Jonathan Martel-Raiche <martel-raiche.jonathan@courrier.uqam.ca>
2021-11-29 21:21:47 -0500

    Modifier le nom de lapp dans server.js 2

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.listen(process.env.PORT || 8080); */
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/meritas-app'));
+app.use(express.static(__dirname + '/dist/meritas-ui'));
 app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname + '/dist/meritas-ui/index.html'));
 });
